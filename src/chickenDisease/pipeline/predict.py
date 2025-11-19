aec109f7d62f190f00f3a4736d744b24b1398cf0
RUPAM DAS <irupamdas1995@gmail.com>
2025-11-20 01:36:37 +0530

    cicd added

--- src/chickenDisease/pipeline/predict.py
+++ src/chickenDisease/pipeline/predict.py
@@ -15,6 +15,7 @@ class PredictionPipeline:
         # load model
         model = load_model(os.path.join("artifacts","training", "model.h5"))
 
+
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
         test_image = image.img_to_array(test_image)
