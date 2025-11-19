54ab50f697eeba3963676367bfc3bf24e3a2fb64
RUPAM DAS <irupamdas1995@gmail.com>
2025-11-20 00:40:56 +0530

    comit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Chiken-Disease-Classification-project
+
+
+<!-- ecr repo -->
+703246780174.dkr.ecr.eu-north-1.amazonaws.com/chicken    
--- src/chickenDisease/pipeline/predict.py
+++ src/chickenDisease/pipeline/predict.py
@@ -24,7 +24,7 @@ class PredictionPipeline:
 
         if result[0] == 1:
             prediction = 'Healthy'
-            return [{ "image" : prediction}]
+            return [{ "this fecal image is " : prediction}]
         else:
             prediction = 'Coccidiosis'
-            return [{ "image" : prediction}]
+            return [{ "this fecal image is " : prediction}]
